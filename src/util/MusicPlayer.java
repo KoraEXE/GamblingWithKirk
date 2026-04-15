1ba46024aa0f3ffb7cb8c9b98c61119a6a82e693
XxAritzxX <vaqueroaritz@gmail.com>
2026-04-15 14:19:19 +0200

    music player para ejecutar y detener la musica en las ventanas

--- src/util/MusicPlayer.java
+++ src/util/MusicPlayer.java
@@ -8,8 +8,24 @@ public class MusicPlayer {
 	
 	private static Clip clip;
 	
-	//public static void play(String rutaArchivo);
-		//stop();//
-	
-	
+	public static void play(String rutaArchivo) {
+        stop(); // Para cualquier música previa
+        try {
+            AudioInputStream audio = AudioSystem.getAudioInputStream(new File(rutaArchivo));
+            clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.loop(Clip.LOOP_CONTINUOUSLY); // Bucle infinito
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.err.println("Error al reproducir música: " + e.getMessage());
+        }
+    }
+
+    /** Detiene y libera la música actual. */
+    public static void stop() {
+        if (clip != null && clip.isRunning()) {
+            clip.stop();
+            clip.close();
+        }
+    }
 }
